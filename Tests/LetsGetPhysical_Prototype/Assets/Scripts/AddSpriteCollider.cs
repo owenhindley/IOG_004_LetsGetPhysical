c8ab3b92a3b2a1daf6a0127a70b4ae25a726b569
Owen Hindley <owen@horizons.studio>
2020-07-21 22:59:28 +0000

    add test project

---
+++ Tests/LetsGetPhysical_Prototype/Assets/Scripts/AddSpriteCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AddSpriteCollider : MonoBehaviour
+{
+    private PolygonCollider2D polygonCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [ContextMenu("AddCollider")]
+    public void AddPolygonCollider(){
+        if (polygonCollider != null){
+            Destroy(polygonCollider);
+        }
+        polygonCollider = gameObject.AddComponent<PolygonCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
