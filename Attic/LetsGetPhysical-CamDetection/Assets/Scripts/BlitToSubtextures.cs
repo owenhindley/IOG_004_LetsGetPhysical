fdcd3fda8fd207beb71de1d4f9617ba51cd2f41d
owenhindley <owenhindley@hotmail.com>
2020-08-15 19:01:29 +0000

    add all the good things

---
+++ Attic/LetsGetPhysical-CamDetection/Assets/Scripts/BlitToSubtextures.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlitToSubtextures : MonoBehaviour
+{
+
+    public GetWebcamTexture getWebcamTexture;
+    
+    [System.Serializable]
+    public class Subtexture {
+        public Material material;
+        public RenderTexture destTexture;
+    }
+
+    public List<Subtexture> subtextures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (getWebcamTexture.webCamTexture != null){
+            if (getWebcamTexture.webCamTexture.didUpdateThisFrame){
+                for (int i=0; i < subtextures.Count; i++){
+                    Graphics.Blit(null, subtextures[i].destTexture, subtextures[i].material);
+                }
+
+            }
+
+        }
+        
+    }
+
+    
+}
---
+++ Attic/LetsGetPhysical-CamDetection/Assets/Scripts/GetWebcamTexture.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetWebcamTexture : MonoBehaviour
+{    
+    public WebCamTexture webCamTexture; 
+    public Renderer renderer;
+
+    public RenderTexture destRT;
+
+    public List<string> devices;
+    private int currentDeviceIndex = 0;
+    public int selectedDevice = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+
+        for (int i=0; i < WebCamTexture.devices.Length; i++){
+            var d = WebCamTexture.devices[i];
+            devices.Add(d.name);
+        }
+        webCamTexture.deviceName = devices[selectedDevice];
+        currentDeviceIndex = selectedDevice;
+        renderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selectedDevice != currentDeviceIndex){
+            webCamTexture.deviceName = devices[selectedDevice];
+            currentDeviceIndex = selectedDevice;
+        }
+
+        if (webCamTexture != null){
+            if (webCamTexture.didUpdateThisFrame){
+                Graphics.Blit(webCamTexture, destRT);
+            }
+        }
+    }
+}
